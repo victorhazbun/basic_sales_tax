8e5b8c257d872baf3a9b0e8f045b45ad1a72c664
vhazbun <vhazbun@getaroom.com>
2023-01-31 18:32:20 -0500

    first commit

---
+++ lib/basic_sales_tax.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "basic_sales_tax/version"
+
+module BasicSalesTax
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/basic_sales_tax/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BasicSalesTax
+  VERSION = "0.1.0"
+end
